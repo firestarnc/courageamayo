7697be3cabac93287c849e0c96cc4be279092036
Courage <115705609+firestarnc@users.noreply.github.com>
2025-11-13 18:01:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+    });
+
+});
